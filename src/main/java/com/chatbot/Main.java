4b4db19f8f4c6cd374dd08134fa2eccc10544306
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-04 17:07:52 -0300

    Implementado método para responder menssagens baseado em palavras chaves

--- src/main/java/com/chatbot/Main.java
+++ src/main/java/com/chatbot/Main.java
@@ -4,14 +4,29 @@ import main.java.com.chatbot.controller.ChatBotController;
 import main.java.com.chatbot.service.ChatBotService;
 import main.java.com.chatbot.service.MessageProcessor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         MessageProcessor processor = new MessageProcessor();
         ChatBotService chatbotService = new ChatBotService(processor);
         ChatBotController chatbotController = new ChatBotController(chatbotService);
 
-        String userMessage = "Olá";
-        String botResponse = chatbotController.receiveMessage(userMessage);
-        System.out.println("Bot: " + botResponse);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bot: Olá! Como posso ajudar?");
+
+        while (true) {
+            System.out.println("Você: ");
+            String userMessage = scanner.nextLine();
+
+            if (userMessage.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            String botResponse = chatbotController.receiveMessage(userMessage);
+            System.out.println("Bot: " + botResponse);
+        }
+        scanner.close();
+
     }
 }
--- src/main/java/com/chatbot/service/MessageProcessor.java
+++ src/main/java/com/chatbot/service/MessageProcessor.java
@@ -1,7 +1,33 @@
 package main.java.com.chatbot.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MessageProcessor {
-    public String process(String userMessage){
-        return "Resposta ao usuário: " + userMessage;
+    private Map<String, String> keywordResponses;
+
+    public MessageProcessor() {
+        keywordResponses = new HashMap<>();
+        initializeResponse();
     }
+
+    private void initializeResponse() {
+        keywordResponses.put("oi", "Olá! Como posso ajudar?");
+        keywordResponses.put("bom dia", "Bom dia! Como posso ajudar?");
+        keywordResponses.put("boa tarde", "Boa Tarde! Como posso ajudar?");
+        keywordResponses.put("boa noite", "Boa Noite! Como posso ajudar?");
+        keywordResponses.put("preciso de ajuda", "Entendi, vou te redirecionar para um atendente.");
+
+    }
+
+    public String process(String userMessage) {
+        for (String keyword : keywordResponses.keySet()) {
+            if (userMessage.toLowerCase().contains(keyword)) {
+                return keywordResponses.get(keyword);
+            }
+        }
+        return "Não consegui identificar!";
+    }
+
+
 }
